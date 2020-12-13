d16c3cdc93ab6eaf0123fec74bee5dad6adde7f0
Aditya Kaushal <kaushal.aditya5nov@gmail.com>
2020-12-13 18:05:46 +0530

    second commit

--- force-app/main/default/lwc/helloWorl/helloWorl.js
+++ force-app/main/default/lwc/helloWorl/helloWorl.js
@@ -1,11 +1,39 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
+import {getRecord} from 'lightning/uiRecordApi';
 import getAccounts from '@salesforce/apex/DisplayAccountsController.getAccounts';
+
+const FIELDS = [
+    'Account.Name',
+    'Account.Industry',
+    'Account.Phone',
+    'Account.Fax',
+];
 
 export default class HelloWorld extends LightningElement {
 
   greeting = 'Salesforce';
+  @api recordId;
   @track accounts;
   @track error;
+  
+  @wire(getRecord, {recordId: '$recordId', fields: FIELDS })
+  account;
+
+    get name() {
+        return this.account.data.fields.Name.value;
+    }
+
+    get industry() {
+        return this.account.data.fields.Industry.value;
+    }
+
+    get phone() {
+        return this.account.data.fields.Phone.value;
+    }
+
+    get fax() {
+        return this.account.data.fields.Fax.value;
+    }
 
   changeHandler(event) {
     this.greeting = event.target.value;
